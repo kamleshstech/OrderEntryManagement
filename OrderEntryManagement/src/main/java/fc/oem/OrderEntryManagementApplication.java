75c3733ac76d6c6cebe2c4bda52913bc8ed84137
TL <TL@gmail.com>
2020-04-27 13:37:33 +0530

    SpringBoot Porject OEM created with required jars

---
+++ OrderEntryManagement/src/main/java/fc/oem/OrderEntryManagementApplication.java
@@ -0,0 +1,13 @@
+package fc.oem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEntryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEntryManagementApplication.class, args);
+	}
+
+}
---
+++ OrderEntryManagement/src/test/java/fc/oem/OrderEntryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.oem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEntryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
